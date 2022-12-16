32952ffc03e7250e6b20f9de78cffd834ff4648a
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-16 17:52:07 +0100

    possible touch function on phones

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 diamonds = document.querySelectorAll('.diamond');
-links = document.getElementsByClassName('link');
+links = document.getElementsByClassName('phone-link');
 
 diamonds.forEach((diamond, index) =>  {
     diamond.addEventListener('mouseover', () => {
@@ -16,5 +16,13 @@ diamonds.forEach((diamond, index) =>  {
 		}
 		links[index].style.width = '0%';
 		links[index].style.opacity = '0';
+	});
+	diamond.addEventListener('ontouchstart', () => {
+		links[index].style.width = '100%';
+		for(i = 0; i < diamonds.length; i++){
+			diamonds[i].style.opacity = '0.5';
+		}
+		diamonds[index].style.opacity = '1';
+		links[index].style.opacity = '0.7';
 	});
 });
