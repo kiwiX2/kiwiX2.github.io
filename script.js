f3abf6e791f8d6f1787f4893b9493bbb6c7b4be5
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-16 23:27:16 +0100

    CHANGERINTATOREOS

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 diamonds = document.querySelectorAll('.diamond');
 textArea = document.getElementsByClassName('phone-link');
 runs = 0;
+linkArray = ["https://kiwix2.github.io", "gallery.html", "https://kiwix2.github.io"]
+
 
 function hoverChanges(index) {
 	runs++;
 	if (isPhone && textArea[index].style.width == '100%' && runs % 2 != 0) {
-		diamonds[index].setAttribute('href', 'https://kiwix2.github.io');
+		diamonds[index].setAttribute('href', linkArray[index]);
 	} else {
 		textArea[index].style.width = '100%';
 		for(i = 0; i < diamonds.length; i++){
-			diamonds[i].style.opacity = '0.5';
+			diamonds[i].style.backgroundColor = '#3d4840';
 		}
-		diamonds[index].style.opacity = '1';
+		diamonds[index].style.backgroundColor = 'var(--asparagus)';
 		textArea[index].style.opacity = '0.7';
 	}
 }
@@ -39,7 +41,7 @@ diamonds.forEach((diamond, index) =>  {
     });
     diamond.addEventListener('mouseout', () => {
         for(i = 0; i < diamonds.length; i++){
-            diamonds[i].style.opacity = '1';
+            diamonds[i].style.backgroundColor = 'var(--ash-gray)';
         }
         textArea[index].style.width = '0%';
         textArea[index].style.opacity = '0';
