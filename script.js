a30000a8426628f6a6891c0c5dee39306def32af
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-16 21:08:20 +0100

    working

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 diamonds = document.querySelectorAll('.diamond');
 textArea = document.getElementsByClassName('phone-link');
+runs = 0;
 
 function hoverChanges(index) {
-	textArea[index].style.width = '100%';
-	for(i = 0; i < diamonds.length; i++){
-		diamonds[i].style.opacity = '0.5';
+	runs++;
+	if (isPhone && textArea[index].style.width == '100%' && runs % 2 != 0) {
+		diamonds[index].setAttribute('href', 'https://kiwix2.github.io');
+	} else {
+		textArea[index].style.width = '100%';
+		for(i = 0; i < diamonds.length; i++){
+			diamonds[i].style.opacity = '0.5';
+		}
+		diamonds[index].style.opacity = '1';
+		textArea[index].style.opacity = '0.7';
 	}
-	diamonds[index].style.opacity = '1';
-	textArea[index].style.opacity = '0.7';
 }
 
 function isPhone() {
@@ -25,23 +31,17 @@ if (isPhone()) {
 }
 
 diamonds.forEach((diamond, index) =>  {
-	diamond.addEventListener('ontouchstart', () => {
+    diamond.addEventListener('touchstart', () => {
 		hoverChanges(index);
-	});
+    });
     diamond.addEventListener('mouseover', () => {
         hoverChanges(index);
     });
-	diamond.addEventListener('mouseout', () => {
-		for(i = 0; i < diamonds.length; i++){
-			diamonds[i].style.opacity = '1';
-		}
-		textArea[index].style.width = '0%';
-		textArea[index].style.opacity = '0';
-	});
-});
-
-textArea.forEach((text, index) => {
-	text.addEventListener('ontouchstart', () => {
-		hoverChanges(index);
-	})
+    diamond.addEventListener('mouseout', () => {
+        for(i = 0; i < diamonds.length; i++){
+            diamonds[i].style.opacity = '1';
+        }
+        textArea[index].style.width = '0%';
+        textArea[index].style.opacity = '0';
+    });
 });
