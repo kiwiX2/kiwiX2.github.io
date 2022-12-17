ea3ff50b50a2056eacd398f3337aa8cfdb45a1c1
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-17 22:03:35 +0100

    seamless scroll integration

--- gallery.js
+++ gallery.js
@@ -7,3 +7,12 @@ function autoScroll() {
 		}
 	})	
 }
+
+scrollValue = 0;
+window.addEventListener('wheel', (event) => {
+	scrollValue = Math.max(300, Math.min(480, scrollValue + event.deltaY / 10));
+	gridItems.forEach((item) => {
+		item.style.transform = 'translateY(' + (300 - scrollValue) + '%)';
+	})
+	console.log(scrollValue);
+})
