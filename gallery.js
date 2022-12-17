35955d8450275157bdc325f9842e82266fcae310
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-17 23:44:27 +0100

    changed scroll speed

--- gallery.js
+++ gallery.js
@@ -12,7 +12,7 @@ function autoScroll() {
 /*-------------------- MANUAL SCROLL --------------------*/
 scrollValue = 0;
 window.addEventListener('wheel', (event) => {
-	scrollValue = Math.max(300, Math.min(480, scrollValue + event.deltaY / 10));
+	scrollValue = Math.max(300, Math.min(480, scrollValue + event.deltaY / 2));
 	gridItems.forEach((item) => {
 		item.style.transform = 'translateY(' + (300 - scrollValue) + '%)';
 	})
