735424ac28cfa2e280352ba0da170374bb77d34c
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-17 21:00:55 +0100

    reduced image size

--- gallery.js
+++ gallery.js
@@ -1,10 +1,9 @@
 gridItems = document.querySelectorAll('.grid-item');
-scrollValue = 0;
 
-window.addEventListener('wheel', (event) => {
-	scrollValue = Math.max(0, Math.min(300, scrollValue + event.deltaY / 10));
+function autoScroll() {
 	gridItems.forEach((item) => {
-		item.style.transform = 'translateY(' + (300 - scrollValue) + '%)';
-	})
-	console.log(scrollValue);
-})
+		for (i = 0; i <= 300; i++) {
+			item.style.transform = 'translateY(' + (300 - i) + '%)';
+		}
+	})	
+}
