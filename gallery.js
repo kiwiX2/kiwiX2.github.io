e83a8b8b3c8d9c0c6e1a60312e8a160c0bc2fc8f
kiwiX2 <120500374+kiwiX2@users.noreply.github.com>
2022-12-18 01:24:32 +0100

    buttons look great!

--- gallery.js
+++ gallery.js
@@ -1,5 +1,11 @@
 gridItems = document.querySelectorAll('.grid-item');
+scrollButton = document.getElementById('scroll-button');
+homeButton = document.getElementById('home-button');
 
+setTimeout(function() {
+    	scrollButton.style.opacity = '1';
+    	homeButton.style.opacity = '1';
+}, 2000);
 
 /*-------------------- AUTO SCROLL --------------------*/
 function autoScroll() {
@@ -10,10 +16,21 @@ function autoScroll() {
 
 
 /*-------------------- MANUAL SCROLL --------------------*/
-scrollValue = 0;
+scrollValue = 300;
 window.addEventListener('wheel', (event) => {
+	if (event.deltaY > 0) {
+		hideButton();
+	}
+	console.log(scrollValue);
 	scrollValue = Math.max(300, Math.min(480, scrollValue + event.deltaY / 2));
 	gridItems.forEach((item) => {
 		item.style.transform = 'translateY(' + (300 - scrollValue) + '%)';
 	})
 })
+
+function hideButton() {
+	scrollButton.style.opacity = '0';
+	setTimeout(function() {
+    	scrollButton.style.visibility = 'hidden';
+	}, 500);
+}
